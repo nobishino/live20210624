a12ab0aac80bedb440f2a4865102b31930ce0f6c
nobishino <nobishii11@gmail.com>
2021-06-24 20:26:47 +0900

    Live end

--- live20210624.go
+++ live20210624.go
@@ -24,11 +24,36 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
+		(*ast.FuncDecl)(nil),
+		(*ast.FuncLit)(nil),
 		(*ast.Ident)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		switch n := n.(type) {
+		case *ast.FuncDecl:
+			if n.Type.Params != nil {
+				for _, arg := range n.Type.Params.List {
+					chType, ok := arg.Type.(*ast.ChanType)
+					if !ok {
+						continue
+					}
+					if chType.Dir == ast.RECV|ast.SEND {
+						pass.Reportf(n.Pos(), "channel argument should be directed")
+					}
+				}
+			}
+			if n.Type.Results != nil {
+				for _, res := range n.Type.Results.List {
+					chType, ok := res.Type.(*ast.ChanType)
+					if !ok {
+						continue
+					}
+					if chType.Dir == ast.RECV|ast.SEND {
+						pass.Reportf(n.Pos(), "channel result should be directed")
+					}
+				}
+			}
 		case *ast.Ident:
 			if n.Name == "gopher" {
 				pass.Reportf(n.Pos(), "identifier is gopher")
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -2,3 +2,23 @@ package a
 
 func f(ch chan int) { // want "channel argument should be directed"
 }
+
+func g(ch <-chan int) {}
+
+func h(ch chan<- int) {}
+
+// 10 min
+
+func i() chan int { // want "channel result should be directed"
+	return nil
+}
+
+func j() <-chan int {
+	return nil
+}
+
+func k() chan<- int {
+	return nil
+}
+
+var x = func(ch chan string) {} // want "channel argument should be directed"
